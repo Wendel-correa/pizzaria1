70aeed5e79fbdbeec7f6addb2987e824abe9e29e
wendel-correa <correa.wendel@gmail.com>
2022-12-14 20:29:29 -0300

    first commit

---
+++ .history/src/main/java/br/com/tech4pizza/pizzaria/controller/PizzaController_20221214150243.java
@@ -0,0 +1,42 @@
+package br.com.tech4pizza.pizzaria.controller;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.tech4pizza.pizzaria.model.Pizza;
+import br.com.tech4pizza.pizzaria.repository.PizzaRepository;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+@RestController
+@RequestMapping("/cardapio")
+public class PizzaController {
+  @Autowired
+  private PizzaRepository repositorio;
+
+  @PostMapping
+  public Pizza cadastrarPizza(@RequestBody Pizza pizza){
+    return repositorio.save(pizza);
+  }
+
+  @GetMapping
+  public List<Pizza> obterCardapio() {
+    return repositorio.findAll();
+  }
+  
+  /* @PutMapping 
+    public Pizza 
+      return entity;
+   
+    @PatchMain
+      Public Pizza
+      */
+
+  }
+
---
+++ .history/src/main/java/br/com/tech4pizza/pizzaria/controller/PizzaController_20221214195335.java
@@ -0,0 +1,49 @@
+package br.com.tech4pizza.pizzaria.controller;
+
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import br.com.tech4pizza.pizzaria.model.Pizza;
+import br.com.tech4pizza.pizzaria.repository.PizzaRepository;
+
+
+@RestController
+@RequestMapping("/cardapio")
+public class PizzaController {
+  @Autowired
+  private PizzaRepository repositorio;
+
+  @PostMapping
+  public Pizza cadastrarPizza(@RequestBody Pizza pizza){
+    return repositorio.save(pizza);
+  }
+
+  @GetMapping
+  public List<Pizza> obterCardapio() {
+    return repositorio.findAll();
+  }
+
+  @GetMapping("/{id}")
+  public Optional<Pizza> obterPizza(@PathVariable String id) {
+    return repositorio.findById(id);
+  }
+
+  @DeleteMapping("/{id}")
+      public void excluirPizza(@PathVariable String id){
+      repositorio.deleteById(id);
+      }
+  
+  @PutMapping ("/{id}")
+      public Pizza atualizarPizza(@Pa){
+      return repositorio.update();
+      }    
+
+  }
+
