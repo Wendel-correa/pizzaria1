c2bd4ed0e64fd5486521c57fa257a5aafa68bcfc
wendel-correa <correa.wendel@gmail.com>
2023-01-09 19:26:50 -0300

    ajustes

--- .history/src/main/java/br/com/tech4pizza/pizzaria/util/Validacao_20230109191906.java
+++ .history/src/main/java/br/com/tech4pizza/pizzaria/util/Validacao_20230109191906.java
@@ -11,13 +11,10 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import com.mongodb.lang.Nullable;
-
 @ControllerAdvice
 public class Validacao extends ResponseEntityExceptionHandler  {
 
     @Override
-    @Nullable
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
             HttpHeaders headers, HttpStatusCode status, WebRequest request) {
         
