6453afb511a4b55080e27b52017e681d8436f56a
wendel-correa <correa.wendel@gmail.com>
2023-01-09 22:03:22 -0300

    MicrosServicos

--- .history/src/main/java/br/com/tech4pizza/pizzaria/PizzariaApplication_20230109214656.java
+++ .history/src/main/java/br/com/tech4pizza/pizzaria/PizzariaApplication_20230109214656.java
@@ -2,10 +2,9 @@ package br.com.tech4pizza.pizzaria;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@Enable
 public class PizzariaApplication {
 
 	public static void main(String[] args) {
